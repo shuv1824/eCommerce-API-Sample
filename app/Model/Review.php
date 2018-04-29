9f2b41d56af13a0c4827fb8df19721c117c3178b
Shah Nawaz Shuvo <shahshuvo@yahoo.com>
2018-04-30 00:21:48 +0600

    First Commit

---
+++ app/Model/Review.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Model\Product;
+
+class Review extends Model
+{
+    public function product(){
+        return $this->belongsTo(Product::Class);
+    }
+}
